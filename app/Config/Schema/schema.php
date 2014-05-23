8b6a7e973cd0dec9f91d27747abc51d13d742efb
Arkadiusz Gac <arkadiusz.gac@gmail.com>
2014-05-24 01:27:15 +0200

    Configure cake

---
+++ app/Config/Schema/schema.php
@@ -0,0 +1,26 @@
+<?php 
+class AppSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $posts = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50),
+		'body' => array('type' => 'text', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+	        'PRIMARY' => array('column' => 'id', 'unique' => true),
+	    ),
+	    'tableParameters' => array(
+	        'engine' => 'InnoDB',
+	        'charset' => 'utf8',
+	    )
+	);
+
+}
--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -105,3 +105,8 @@ CakeLog::config('error', array(
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
 	'file' => 'error',
 ));
+
+/**
+ * Load DebugKit
+ */
+CakePlugin::load('DebugKit');
